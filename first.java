111706ae22abf5c919f4334fc49769453ffe7e94
Basava Chari Boppudi <basavachariboppudi@gmail.com>
2022-01-19 21:18:41 +0530

    modified first.java in new branch

--- first.java
+++ first.java
@@ -3,5 +3,9 @@ package mynewrepo;
 class first{
     public static void main(String[] args) {
     System.out.println("this is my first repo");   
+    System.out.println(first.add(5,4));
+}
+    public static int add(int a,int b){
+        return a+b;
     }
 }
